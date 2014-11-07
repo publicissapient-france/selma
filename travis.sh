71d2b287850c4ce1c442d81f161a05a0d892bf92
slemesle <slemesle@xebia.fr>
2014-11-08 00:43:58 +0100

    Add deploy for openjdk7

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 env
 
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${DEPLOY} = 'true' ]; then
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_JDK_VERSION} = 'openjdk7' ]; then
     mvn clean deploy --settings target/travis/settings.xml
 else
     mvn clean verify --settings target/travis/settings.xml
