49a7454f385fbde01884d9dd21c7f241e1b43d6c
Semiao Marco <marco.semiao@gmail.com>
2016-08-31 01:02:04 +0200

    je n'ai pas les droits pour deployer avec le nom de domaine de xebia

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 env
 
-if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_JDK_VERSION} = 'openjdk7' ]; then
+if [ ${TRAVIS_PULL_REQUEST} = 'false' ]; then
     mvn clean deploy --settings target/travis/settings.xml
 else
     mvn clean verify --settings target/travis/settings.xml
