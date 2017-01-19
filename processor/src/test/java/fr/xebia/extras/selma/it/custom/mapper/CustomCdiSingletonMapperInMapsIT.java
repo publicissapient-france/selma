adb9318450bc72eb764944f2d8d60ba78d7440b9
Musikolo <musikolo@hotmail.com>
2017-01-18 20:43:00 -0600

    Use of CustomCdiSingletonMapperInMaps instead of CustomCdiMapperInMaps

--- processor/src/test/java/fr/xebia/extras/selma/it/custom/mapper/CustomCdiSingletonMapperInMapsIT.java
+++ processor/src/test/java/fr/xebia/extras/selma/it/custom/mapper/CustomCdiSingletonMapperInMapsIT.java
@@ -1,9 +1,18 @@
-/*
- * Copyright 2013 Séven Le Mesle Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
- * applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+/*-
+ * Copyright 2013 Xebia and Séven Le Mesle
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 package fr.xebia.extras.selma.it.custom.mapper;
 
@@ -18,12 +27,12 @@ import fr.xebia.extras.selma.it.utils.IntegrationTestBase;
 
 @Compile(withClasses = CustomCdiSingletonMapperInMaps.class)
 public class CustomCdiSingletonMapperInMapsIT extends IntegrationTestBase {
-	
-	@Test
-	public void given_cdi_mapper_should_be_annotated_with_Inject_and_Singleton() {
-		
-		final CustomCdiMapperInMaps mapper = Selma.getMapper(CustomCdiMapperInMaps.class);
-		assertThat(mapper.getClass().getAnnotation(javax.inject.Named.class), notNullValue());
-		assertThat(mapper.getClass().getAnnotation(javax.inject.Singleton.class), notNullValue());
-	}
+    
+    @Test
+    public void given_cdi_mapper_should_be_annotated_with_Inject_and_Singleton() {
+        
+        final CustomCdiSingletonMapperInMaps mapper = Selma.getMapper(CustomCdiSingletonMapperInMaps.class);
+        assertThat(mapper.getClass().getAnnotation(javax.inject.Named.class), notNullValue());
+        assertThat(mapper.getClass().getAnnotation(javax.inject.Singleton.class), notNullValue());
+    }
 }
